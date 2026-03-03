131050df115411888394dea1bae3573c0b0c570d
Thảo <thao205206@gmail.com>
2026-03-03 10:11:58 +0700

    Add files via upload

---
+++ src/abstract/FileHandler.java
@@ -0,0 +1,15 @@
+package hrdemo;
+
+
+import java.util.*;
+
+public class FileHandler {
+
+    public void saveEmployees(List<Employee> list) {
+        // TODO: Ghi file (ObjectOutputStream / CSV)
+    }
+
+    public List<Employee> loadEmployees() {
+        return new ArrayList<>();
+    }
+}
